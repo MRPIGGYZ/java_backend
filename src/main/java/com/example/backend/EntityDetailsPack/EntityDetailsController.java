816adcf86845595293ac84c939c736b7f7a895fc
zhenghp <973196592@qq.com>
2021-09-09 00:40:53 +0000

    增加实体详情搜索的鲁棒性，现在course和category不是非必须的了。实体详情请求总共有以下几种情况：若从实体搜索页面进入，则在实体搜索时便获得了四个参数，直接传入请求即可。若从收藏或历史记录页面进入，由于我在后端保存了这四个参数，所以也可以直接传入（和实体搜索页面进入相同）。若从问答机器人或实体详情关联进入，则只能获得label和uri两个参数，因此category和course参数传入空字符串即可，后端会自动搜索处理。

--- src/main/java/com/example/backend/EntityDetailsPack/EntityDetailsController.java
+++ src/main/java/com/example/backend/EntityDetailsPack/EntityDetailsController.java
@@ -3,6 +3,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONPOJOBuilder;
+import com.example.backend.GetInstanceByUriPack.GetCourseName;
 import com.example.backend.GetInstanceByUriPack.GetInstanceByUri;
 import com.example.backend.PersonalInterface.BackendLogin;
 import com.example.backend.PersonalInterface.QuickMap;
@@ -22,10 +23,11 @@ public class EntityDetailsController implements SimplifyData {
     @Autowired
     private UserDao userDao;
     @GetMapping(path="")
-    public @ResponseBody JSONObject EntityDetails (HttpServletRequest req, @RequestParam String course
-            , @RequestParam String label, @RequestParam String uri, @RequestParam String category) {
+    public @ResponseBody JSONObject DirectForEntityDetails (HttpServletRequest req, @RequestParam String course
+            , @RequestParam String label, @RequestParam String uri, @RequestParam String category) { //如果没有course或category，请在相应位置传入空字符串。
         RestTemplate restTemplate = new RestTemplate();
         JSONObject returnValue = new JSONObject();
+        if (course.equals("")) course = GetCourseName.GetCourseNameInUri(uri);
         String url = "http://open.edukg.cn/opedukg/api/typeOpen/open/infoByInstanceName?course={course}&name={name}&id={id}";
         if (id == null) id = BackendLogin.getOpeneduID();
         JSONObject response = restTemplate.getForObject(url, JSONObject.class, QuickMap.createMap("course", course, "name", label, "id", id));
@@ -33,6 +35,9 @@ public class EntityDetailsController implements SimplifyData {
             JSONObject gooddata = GetInstanceByUri.GetRawData(uri);
             JSONObject data = (JSONObject) JSON.toJSON(response.get("data"));
             data = Simplify(data, gooddata); //这里有待修改，使用gooddata拼接是不合适的，之后需要完善对data的过滤函数
+            if (category.equals("")) {
+                category = getCategory(data.getJSONArray("property"), label);
+            }
             data.put("category", category);
             data.put("course", course);
             data.put("uri", uri);
@@ -60,4 +65,13 @@ public class EntityDetailsController implements SimplifyData {
         return returnValue;
     }
 
+    private String getCategory (JSONArray property, String label) {
+        for (int i=0; i<property.size(); i++) {
+            JSONObject data = property.getJSONObject(i);
+            if (data.getString("feature_key").equals("分类")) {
+                return data.getString("feature_value");
+            }
+        }
+        return label;
+    }
 }
